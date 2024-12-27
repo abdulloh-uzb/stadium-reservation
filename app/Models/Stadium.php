ccda0073b7c00ff9600815d90d16823fc250463f
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2024-12-27 15:58:05 +0500

    added mutator to decode images json

--- app/Models/Stadium.php
+++ app/Models/Stadium.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Stadium extends Model
 {
-
     use HasFactory;
 
     protected $table = 'stadium';
     protected $fillable = ["name", "description", "location", "price", "images","open_time", "close_time"];
+    
+    protected function images(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => json_decode($value)
+        );
+    }
+
 }
