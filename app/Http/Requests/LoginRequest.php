2fbdc7e6caa5df2811dccbc9eaa590edae1e650e
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2025-01-20 23:57:18 +0500

    bug fixed

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -7,14 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class LoginRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -22,8 +14,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ["required"],
-            ["email"],
+            'email' => ["required", "email"],
             "password" => ["required"]
         ];
     }
